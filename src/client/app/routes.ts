137ecfbc95de19e88d209d40f6ef2cfc745e7de7
markpock <markpock7@gmail.com>
2025-04-05 05:27:39 -0700

    the cull is almost concluded

--- src/client/app/routes.ts
+++ src/client/app/routes.ts
@@ -1,6 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), 
+export default [
+    index("routes/home.tsx"), 
     route("editor", "routes/editor.tsx"),
     route("ImageEditor", "routes/ImageEditor.tsx")
 ] satisfies RouteConfig;
